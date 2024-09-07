31c2995c7c2d09b2f818c3e6ff75b88f743527d3
Gierado Pham <gieradopham25@Gierados-MBP.home>
2024-09-07 11:02:09 -0700

    Migrated project to React framework

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Gierado Pham</h1>
+        <p>Aerospace Engineering</p>
+      </header>
+      <section className="section">
+        <h2>About Me</h2>
+        <p>Brief description.</p>
+      </section>
+      <section className="section">
+        <h2>Experience</h2>
+        <p>Details about professional experience.</p>
+      </section>
+      <section className="section">
+        <h2>Education</h2>
+        <p>Educational Background.</p>
+      </section>
+      <section className="section">
+        <h2>Projects</h2>
+        <p>Technical Projects.</p>
+      </section>
+      <footer>
+        <p>Contact info and links to LinkedIn and GitHub</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
